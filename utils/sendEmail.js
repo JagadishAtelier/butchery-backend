b643285af9ba78200c97346925e897fa1b8ec68b
gnanaparthiban <gnanaparthiban@xtowns.in>
2025-10-11 19:33:09 +0530

    smtp ssl

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -16,8 +16,8 @@ const initTransporter = () => {
 
     transporter = nodemailer.createTransport({
       host: process.env.EMAIL_HOST || 'smtp.gmail.com',
-      port: process.env.EMAIL_PORT || 587,
-      secure: false, // true for port 465
+      port: process.env.EMAIL_PORT || 465,
+      secure: true, // true for port 465
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
